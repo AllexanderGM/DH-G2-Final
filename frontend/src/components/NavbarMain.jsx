664dc5248fc0d2ba1e130162714b1edeb4d5a95b
Andres Ferrada <an.ferrada@gmail.com>
2025-02-19 18:11:20 -0300

    add: crear menú hamburguesa para móviles / disminuir tamaño de links del Navbar en pantalla media y pequeña

--- frontend/src/components/NavbarMain.jsx
+++ frontend/src/components/NavbarMain.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import { Navbar, NavbarBrand, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, NavbarContent, NavbarItem, Link, Image } from '@heroui/react'
 
 import BrandButton from './BrandButton.jsx'
 import img from '../assets/Logo/logo_navbar/svg/isotipo_sm.svg'
 
 function NavbarMain() {
-  const menuItems = ['Lugares', 'Guias', 'Blog', 'Crear Usuario', 'Iniciar Sesion']
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const menuItems = ['Somos', 'Tours', 'Contacto']
 
   /*Primera parte es de Web, la segunda parte es Mobil*/
   return (
     <Navbar
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
       classNames={{
         wrapper: 'max-w-6xl mx-auto'
       }}>
@@ -18,31 +22,36 @@ function NavbarMain() {
           <p className="font-bold text-inherit">Glocal Tour</p>
         </Link>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent className="hidden sm:flex  gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" className="hover:text-red-600" href="#">
+          <Link color="foreground" className="sm:text-sm md:text-base hover:text-red-600" href="#">
             Somos
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" className="hover:text-red-600" href="#">
+          <Link color="foreground" className="hover:text-red-600 sm:text-sm md:text-base" href="#">
             Tours
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" className="hover:text-red-600" href="#">
+          <Link color="foreground" className="hover:text-red-600 sm:text-sm md:text-base" href="#">
             Contacto
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Crear usuario</Link>
+        <NavbarItem className="lg:flex text-sm">
+          <Link className="text-sm md:text-base" href="#">
+            Crear usuario
+          </Link>
         </NavbarItem>
         <NavbarItem>
-          <BrandButton color="brandColor">Iniciar sesión</BrandButton>
+          <BrandButton color="brandColor" className="text-sm md:text-base">
+            Iniciar sesión
+          </BrandButton>
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} className="sm:hidden" />
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
