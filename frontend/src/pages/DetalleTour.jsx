cd843a1f9112003f7449a4aee4bbb642cbfeacd7
Andres Ferrada <an.ferrada@gmail.com>
2025-02-23 14:19:47 -0300

    style: mejorar grilla de galería

--- frontend/src/pages/DetalleTour.jsx
+++ frontend/src/pages/DetalleTour.jsx
@@ -42,6 +42,10 @@ const DetalleTour = () => {
       {
         imageId: 4,
         src: 'https://images.unsplash.com/photo-1598313795136-a202370958af?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      },
+      {
+        imageId: 5,
+        src: 'https://images.unsplash.com/photo-1504359147064-a13220c2ea38?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       }
     ]
     : []
@@ -51,7 +55,9 @@ const DetalleTour = () => {
       {tour ? (
         <>
           <div className="flex justify-between items-center h-[100px]">
-            <h1>{tour.destino}</h1>
+            <h1>
+              {tour.nombre}. {tour.destino}
+            </h1>
             <Link to="/">
               <span
                 className="material-symbols-outlined text-2xl text-gray-500 transition-all duration-200 hover:text-gray-900 hover:text-3xl"
@@ -60,9 +66,17 @@ const DetalleTour = () => {
               </span>
             </Link>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ga">
-            {pseudoGallery.map(image => (
-              <Image key={image.imageId} src={image.src} />
+          <div className="grid grid-flow-row grid-cols-2 md:grid-flow-col md:grid-cols-3 md:grid-rows-2 gap-1 md:h-[400px] mb-10">
+            {pseudoGallery.map((image, index) => (
+              <div key={image.imageId} className={`overflow-hidden flex justify-center items-center w-full first:row-span-2`}>
+                <Image
+                  src={image.src}
+                  classNames={{
+                    wrapper: `w-full ${index === 0 && 'h-full'}`,
+                    img: `w-full ${index === 0 && 'h-full rounded-none rounded-tl-xl rounded-bl-xl'} object-cover`
+                  }}
+                />
+              </div>
             ))}
             {/* <Image src={tour.imagenes[0]} alt={tour.destino} className="w-full h-80 object-cover rounded-lg my-4" /> */}
           </div>
