c2e1cf65ba9ce535a1026cca18c200c3880c53df
Adriana Cadavid <adrianalcadavid5@gmail.com>
2025-03-01 19:34:12 -0500

    agregando footer al proyecto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import NavbarMain from '@components/NavbarMain'
+import Footer from '@components/Footer'
 
 function App() {
   return (
     <>
       <NavbarMain />
       <Outlet />
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import img from '../assets/Logo/logo_footer/isotipo_sm 1.png'
+import inst from '../assets/Logo/logo_footer/instagram.png'
+import faceb from '../assets/Logo/logo_footer/faceb.png'
+import twiter from '../assets/Logo/logo_footer/twiter.png'
+import github from '../assets/Logo/logo_footer/github.png'
+const Footer = () => {
+  return (
+    <footer className="w-full bg-white text-black py-6 px-8 border-t flex flex-col md:flex-row md:justify-between md:items-center">
+      {/* Logo alineado a la izquierda */}
+      <div className="flex items-center md:flex-grow justify-center md:justify-start">
+        <img src={img} alt="Glocal Tour isotipo" className="h-10" />
+      </div>
+
+      {/* Texto centrado */}
+      <div className="flex md:flex-grow justify-center">
+        <span className="text-sm md:text-base font-medium">© Glocal Tour. {new Date().getFullYear()}</span>
+      </div>
+
+      {/* Redes sociales alineadas a la derecha */}
+      <div className="flex flex-1 items-center justify-center md:justify-end space-x-4 mt-4 md:mt-0">
+        <img src={inst} alt="Instagram" className="h-6 cursor-pointer transition-all duration-200 hover:shadow-md hover:-translate-y-1" />
+
+        <img src={faceb} alt="Facebook" className="h-6 cursor-pointer transition-all duration-200 hover:shadow-md hover:-translate-y-1" />
+
+        <img src={twiter} alt="Twitter" className="h-6 cursor-pointer transition-all duration-200 hover:shadow-md hover:-translate-y-1" />
+
+        <img src={github} alt="GitHub" className="h-6 cursor-pointer transition-all duration-200 hover:shadow-md hover:-translate-y-1" />
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
