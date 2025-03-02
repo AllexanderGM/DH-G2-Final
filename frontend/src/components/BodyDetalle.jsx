277f06b7866832e57e176ed59828d6eeb3560ebc
Andres Ferrada <an.ferrada@gmail.com>
2025-03-02 00:23:56 -0300

    add: características en Detalles

--- frontend/src/components/BodyDetalle.jsx
+++ frontend/src/components/BodyDetalle.jsx
@@ -1,10 +1,70 @@
 import CardDetalle from './CardDetalle.jsx'
 
 const BodyDetalle = ({ tour }) => {
+  const incluye = [
+    { icon: 'villa', tag: 'Alojamiento' },
+    { icon: 'directions_bus', tag: 'Transporte' },
+    { icon: 'confirmation_number', tag: 'Boletos' },
+    { icon: 'icecream', tag: 'Snack' },
+    { icon: 'water_full', tag: 'Bebidas' },
+    { icon: 'breakfast_dining', tag: 'Desayuno' },
+    { icon: 'ramen_dining', tag: 'Almuerzo' },
+    { icon: 'restaurant', tag: 'Cena' },
+    { icon: 'familiar_face_and_zone', tag: 'Guía' },
+    { icon: 'local_hospital', tag: 'Seguro de vida' },
+    { icon: 'downhill_skiing', tag: 'Actividades' },
+    { icon: 'photo_camera', tag: 'Fotografías' },
+    { icon: 'redeem', tag: 'Souvenirs' },
+    { icon: 'roller_skating', tag: 'Equipamiento' },
+    { icon: 'rss_feed', tag: 'Wifi' },
+    { icon: 'savings', tag: 'Propinas' },
+    { icon: 'accessibility_new', tag: 'Asistencia' }
+  ]
+
   return (
     <div>
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
         <div className=" lg:col-span-2 pr-20 pl-0 text-[0.9rem]">
+          <h2 className="text-[1rem] font-bold mb-4">Incluye</h2>
+          <div className="grid grid-cols-3">
+            <div className="flex justify-start items-center pb-2">
+              <span className="material-symbols-outlined text-2xl pr-3" width="100%">
+                {incluye[0].icon}
+              </span>
+              <p>{incluye[0].tag}</p>
+            </div>
+            <div className="flex justify-start items-center pb-2 ">
+              <span className="material-symbols-outlined text-2xl pr-3 " width="100%">
+                {incluye[5].icon}
+              </span>
+              <p>{incluye[5].tag}</p>
+            </div>
+            <div className="flex justify-start items-center pb-2 ">
+              <span className="material-symbols-outlined text-2xl pr-3 " width="100%">
+                {incluye[3].icon}
+              </span>
+              <p>{incluye[3].tag}</p>
+            </div>
+            <div className="flex justify-start items-center pb-2 ">
+              <span className="material-symbols-outlined text-2xl pr-3 " width="100%">
+                {incluye[6].icon}
+              </span>
+              <p>{incluye[6].tag}</p>
+            </div>
+            <div className="flex justify-start items-center pb-2 ">
+              <span className="material-symbols-outlined text-2xl pr-3 " width="100%">
+                {incluye[12].icon}
+              </span>
+              <p>{incluye[12].tag}</p>
+            </div>
+            <div className="flex justify-start items-center pb-2 ">
+              <span className="material-symbols-outlined text-2xl pr-3" width="100%">
+                {incluye[8].icon}
+              </span>
+              <p>{incluye[8].tag}</p>
+            </div>
+          </div>
+          <hr className="bordegray border-solid border-gray-300 mt-8 mb-8" />
           <div>
             <h2 className="text-[1rem] font-bold mb-4">Descripción del lugar</h2>
             <p className="text-gray-800">
