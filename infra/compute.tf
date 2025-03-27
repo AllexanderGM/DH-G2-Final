32bcf9dffea7fa8b8fd2e073fbc2488e57ecb6f6
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2025-03-27 13:57:28 -0500

    add: se agrega la creación de llaves privadas y publicas desde terraform para la instacia EC2

--- infra/compute.tf
+++ infra/compute.tf
@@ -1,29 +1,21 @@
+# 🔹 
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "${path.module}/.generated_key.pem"
+  file_permission = "0600"
+}
+
 # 🔹 EC2 para backend
 module "ec2" {
-  source            = "terraform-aws-modules/ec2-instance/aws"
-  version           = "5.7.1"
-  name              = "${var.prefix}-backend"
-  instance_type     = "t2.micro"
-  ami               = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (ajusta la AMI según región)
-  key_name          = "mi-key"
-  vpc_id            = module.vpc.vpc_id
-  subnet_id         = module.vpc.private_subnet_ids[0] # Ahora usa el ID generado por el módulo
-  security_group_id = module.security_groups.app_sg_id
-  prefix            = var.prefix
-
-  user_data = <<-EOF
-    #!/bin/bash
-    amazon-linux-extras enable docker
-    yum install -y docker
-    systemctl start docker
-    systemctl enable docker
-    usermod -aG docker ec2-user
-
-    # Iniciar sesión en Docker Hub (si es necesario)
-    echo "${var.docker_registry_password}" | docker login -u "${var.docker_registry_user}" --password-stdin
-
-    # Descargar y ejecutar la imagen de Docker
-    docker pull ${var.docker_image_backend}
-    docker run -d -p 80:3000 ${var.docker_image_backend}
-  EOF
+  source                  = "terraform-aws-modules/ec2-instance/aws"
+  version                 = "5.7.1"
+  name                    = "${var.prefix}-backend"
+  instance_type           = "t2.micro"
+  ami                     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (ajusta la AMI según región)
+  key_name                = aws_key_pair.generated.key_name
+  vpc_id                  = module.vpc.vpc_id
+  vpc_security_group_ids  = [module.security_groups.app_sg_id]
+  subnet_id               = module.vpc.private_subnet_ids[0] # Ahora usa el ID generado por el módulo
+  prefix                  = var.prefix
+  tags                    = { Name = "${var.prefix}-backend" }
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -84,3 +84,18 @@ variable "docker_image_backend" {
   description = "Imagen de Docker para el backend"
   type        = string
 }
+
+variable "key_name" {
+  description = "Nombre del key pair para la instancia EC2"
+  type        = string
+}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated" {
+  key_name   = "${var.prefix}-ec2-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
